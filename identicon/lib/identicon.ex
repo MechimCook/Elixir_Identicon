80fb9865b8b1ea3096c88c30dbb0f089a86c45da
mechim <mechim@getlua.com>
2018-09-18 14:57:10 -0400

    end of lecture 41

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -13,9 +13,7 @@ defmodule Identicon do
     %Identicon.Image{hex: hex}
   end
 
-  def pick_color(image) do
-    %Identicon.Image{hex: hex_list} = image
-    [r,g,b | _tail]=hex_list
-    [r,g,b]
+  def pick_color(%Identicon.Image{hex: [r,g,b | _tail]} = image) do
+    %Identicon.Image{image | color: {r,g,b}}
   end
 end
